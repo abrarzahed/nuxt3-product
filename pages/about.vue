3e1a1430cb2c3871412f773fea6f420a7331b5c4
abrarzahed <abrarzahed6986@gmail.com>
2022-11-10 14:09:00 +0600

    created layout and added tailwindcss

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus autem
+      accusantium, eos fugiat quos quasi aperiam voluptatem repellat quia qui?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus autem
+      accusantium, eos fugiat quos quasi aperiam voluptatem repellat quia qui?
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
